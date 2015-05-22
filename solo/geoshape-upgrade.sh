919acb2c9b5ac50a543d97a873dbe93a0b3c6909
Syrus Mesdaghi <syrusm@gmail.com>
2015-05-22 16:07:18 +0000

    added provision script to support applying dna.json easily

--- solo/geoshape-upgrade.sh
+++ solo/geoshape-upgrade.sh
@@ -89,11 +89,8 @@ berks update
 rm -rf /opt/chef-run/cookbooks
 berks vendor /opt/chef-run/cookbooks
 
-# Note: cannot be in the /opt/rogue-chef-repo when running this command.
-#       ruby shadow install failes because chef failes to find some 
-#       packages such as pg and shadow
-cd /opt/chef-run
-chef-solo -c /opt/chef-run/solo.rb -j /opt/chef-run/dna.json
+# now that we have the right commit of repositories checked out, we can provision
+./geoshape-provision.sh
 
 # restore stored directory
 popd
